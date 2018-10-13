d844361c243d3d49f2c8c09d3b4143d1de879b9a
punith <akshay0696@gmail.com>
2018-10-13 15:22:39 +0530

    Changed hardcoded message

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function(){
     console.log('Connected to the server');
-
-    socket.emit('createMessage',{
-        from:'bogo@example.com',
-        text:'yup, that worked for me'
-    })
 });
 
 
